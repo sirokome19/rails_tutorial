1e96c9c6157576e1f8415192d73391a803ad9a00
sirokome19 <sirokome19@toki.waseda.jp>
2020-10-02 02:55:50 +0900

    user login test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,18 +1,35 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-    test "login with invalid information" do
-      # GET PAGE
-      get login_path
-      assert_template 'sessions/new'
+  def setup
+    @user = users(:michael)
+  end
+  test "login with invalid information" do
+    # GET PAGE
+    get login_path
+    assert_template 'sessions/new'
+    
+    # try wrong login
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty? # need flash
       
-      # try wrong login
-      post login_path, params: { session: { email: "", password: "" } }
-      assert_template 'sessions/new'
-      assert_not flash.empty? # need flash
-        
-      # go another page
-      get root_path
-      assert flash.empty? # needless flash
-    end
+    # go another page
+    get root_path
+    assert flash.empty? # needless flash
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+      # login_pathがなくなっているか
+    assert_select "a[href=?]", logout_path
+      # logout_pathがあるか
+    assert_select "a[href=?]", user_path(@user)
+  end
 end
