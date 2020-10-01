de2e433c892df004a5cde624317dd8d8bfe4b69a
sirokome19 <sirokome19@toki.waseda.jp>
2020-10-01 16:28:54 +0900

    valid name and email presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    # name, email can't be blank
+    validates :name, presence: true
+    validates :email, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+  test "name should be present" do
+    # name can't be blank
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+  test "email should be present" do
+    # email can't be blank
+    @user.email = "     "
+    assert_not @user.valid?
+  end
 end
