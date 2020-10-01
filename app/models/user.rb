fcc372f1a6e0ef34934371e15d5ec5c88a9e71c5
sirokome19 <sirokome19@toki.waseda.jp>
2020-10-01 16:40:55 +0900

    valid email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     # name, email can't be blank
-    validates :name, presence: true
-    validates :email, presence: true
+    validates :name, presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                        format: { with: VALID_EMAIL_REGEX }
+
 end
